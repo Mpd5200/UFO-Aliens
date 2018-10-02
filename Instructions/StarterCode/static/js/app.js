781949ac829ac6f31b4b9c0ac8096b27e6ad2e0b
Michael Duffner <michaelduffner@Michaels-MacBook-Pro-4.local>
2018-10-02 12:52:26 -0400

    fixed

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,46 +1,42 @@
-// from data.js
+
 var tableData = data;
+
 
 var tbody = d3.select("tbody");
 
-//print the data ato the console
-console.log(data);
-
-//insert the JSON objects into the table tbody
 function insertData(data){
-  var row =tbody.append("tr");
-  Object.entries(data).forEach(([key,value])=>{
-    var cell = tbody.append("td");
-    cell.text(value.toString());
-  })
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+        var cell = tbody.append("td");
+        cell.text(value.toString());
+    })
 };
 
 tableData.forEach(insertData);
 
-function clearData(filterData){
-  d3.select("tbody").html("");
-  filterData.forEach(insertData);
+
+
+function clearData(filteredData){
+    d3.select("tbody").html("");
+    filteredData.forEach(insertData);
 };
 
-var button = d3.select("#filter-btn")
-var inputBar = d3.select("form-control")
+// Filtering by date
+var button = d3.select("#filter-btn");
+var inputBar = d3.select(".form-control");
 
-function handleClick(){
-  console.log("Date filter was selected");
-  console.log("d3.event.target");
-};
+button.on("click", function (){
+    d3.event.preventDefault();
+    var inputValue = inputBar.property("value");
+    console.log(inputValue);
+    var filteredData = tableData.filter(data => data.datetime == inputValue);
 
-button.on("click",function(){
-d3.event.preventDefault();
-var inputValue = inputBar.property("datetime");
-console.log(inputValue);
-var filterData = tableData.filter(data => data.datetime == inputValue);
-
-clearData(filterdata);
+    clearData(filteredData);
 })
 
-function show_more(element_to_show){
-  var element_to_show = document.getElementById("more");
-}
+function show_more ( element_to_show ) {
+    var element_to_show = document.getElementById("more");
+    element_to_show.style.display = "inline";
+    }
 
-console.log(inputBar)
+console.log(inputBar);
